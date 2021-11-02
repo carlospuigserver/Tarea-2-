9fcec8d32f10c477cfab9eb87dc7585fe7d04a3f
carlospuigserver <cpuiggut@myuax.com>
2021-11-02 23:34:49 +0100

    subida

--- Juego1.py
+++ Juego1.py
@@ -8,15 +8,15 @@ intento=1
 while numero!=cpu:
     while numero<0 or numero>100:
         print("No puedes jugar con este número")
-        numero=int(input("Dame un número (0-99"))
+        numero=int(input("Dame un número (0-99)"))
         intento=intento+1
     if numero<cpu:
         print("El número que has elegido es demasiado pequeño")
-        numero=int(input("Dame un número (0-99"))
+        numero=int(input("Dame un número (0-99)"))
         intento=intento+1
     else:
         print("El número que has elegido es demasiado grande")
-        numero=int(input("Dame un número (0-99"))
+        numero=int(input("Dame un número (0-99)"))
         intento=intento+1
 else:
     print("Felicidades, has conseguido finalizar el juego")
